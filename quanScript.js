c1bcba7f78c417a10a223160dee8ffea6dbb1acf
Nguyễn Chí Hiếu <82454902+SCKurumi@users.noreply.github.com>
2021-11-24 23:08:17 +0700

    Fix js và làm cái nút chuyển đổi rõ hơn

--- quanScript.js
+++ quanScript.js
@@ -12,43 +12,77 @@ menuButton.addEventListener("click", () => {
 
 function showMenu()
 {
-    if(isOpened){
-        sidemenu.style.left = "-250px";
-        section.style.marginLeft = "0px";
-        isOpened = false;
-    }else
-    {
-        sidemenu.style.left = "0px";
-        section.style.marginLeft = "250px";
-        isOpened = true;
-    }
+  [sidemenu.style.left, section.style.marginLeft, isOpened] = [
+    isOpened ? "-250px" : "0px",
+    isOpened ? "0px" : "250px",
+    !isOpened
+  ];
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
+
+
+var currentSlidePos = 1;
+showSlideAt(currentSlidePos);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function moveSlides(value) {
+  const slides = document.getElementsByClassName("mySlides");
+  const totalSlide = slides.length;
+  if (totalSlide === 0) return;
+
+  let newSlidePos = currentSlidePos + value;
+  if (newSlidePos > totalSlide - 1) {
+    newSlidePos = 0;
+  } else if (newSlidePos < 0) {
+    newSlidePos = totalSlide - 1;
+  }
+
+  currentSlidePos = newSlidePos;
+   
+  showSlideAt(newSlidePos);
 }
+
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
+function showSlideAt(pos) {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  if (slides.length === 0) return;
+
+  Array.from(slides).forEach((slide, index) => {
+    slide.style.display = "none";
+    dots[index].className = dots[index].className.replace(" active", "");
+  })
+
+  slides[pos].style.display = "block";
+  dots[pos].className += " active";
 }
