43578312c5f21e843b11cb02eec755f3972f6ad4
C0rvo <quan.uminh1@gmail.com>
2021-11-25 21:12:25 +0700

    Add quang part

--- quanScript.js
+++ quanScript.js
@@ -52,3 +52,18 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+var QQfont = document.querySelectorAll("body");
+for(var i = 0;i < QQfont.length; i++){
+    QQfont[i].classList.add('QQfont');
+}
+
+var Qfont = document.querySelectorAll(".title");
+for(var i = 0;i < QQfont.length; i++){
+    Qfont[i].classList.add('Qfont');
+}
+
+var Qbtn = document.querySelectorAll("button");
+for(var i = 0;i < Qbtn.length; i++){
+    Qbtn[i].classList.add('Qbtn');
+}
